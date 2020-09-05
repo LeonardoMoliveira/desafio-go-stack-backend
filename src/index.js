f5d660615e4c71df0bc1809b133e9feb96e0641e
leonardoMoliveira <leonardo.m.oliveira98@gmail.com>
2020-09-05 20:49:02 -0300

    finish CRUD methods

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (request, response) => {
-  /* const { title, owner } = request.query;
+  const { title } = request.query;
 
-  console.log(title);
-  console.log(owner); */
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
 
-  return response.json(projects);
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
@@ -28,6 +29,7 @@ app.post('/projects', (request, response) => {
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
   const projectIndex = projects.findIndex((project) => project.id === id);
 
@@ -35,11 +37,29 @@ app.put('/projects/:id', (request, response) => {
     return response.status(400).json({ error: 'Project not found' });
   }
 
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
   return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json(['Projeto 05', 'Projeto 06']);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
